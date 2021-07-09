e49db2be9c9c68d0d3557f3955ec2d5b67a76577
Glen Borkowski <glen.borkowski@bloomreach.com>
2021-07-09 09:14:09 -0400

    updated run.sh

--- run.sh
+++ run.sh
@@ -7,12 +7,7 @@ export CLASSPATH=$CLASSPATH:$BASE/../log4j2.xml
 export CLASSPATH=$CLASSPATH:$BASE/transformer-1.0-SNAPSHOT-jar-with-dependencies.jar
 
 # There is one required argument for each phase:
-# specify the sub directory you want to use
-# best practices say to use customer name:
-#
-# ../properties/Graybar/importPhase.properties
-#
-# for the above, you would use Graybar as the argument
+# specify the customer you want to use
 
 #java com.br.transform.ImportPhase Haberkorn
 #java com.br.transform.TransformPhase Haberkorn
